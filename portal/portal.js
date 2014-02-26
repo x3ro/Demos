2c5458b44d41b58aff4f07f16cb7471bceaa54c8
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-02-26 13:52:46 +0100

    Added the correct line parser to the portal

--- portal/portal.js
+++ portal/portal.js
@@ -8,7 +8,7 @@
 /**
  * @fileoverview    This portal acts as a backend of a reduced border router for the RIOT cebit demo.
  * 
- * @date			Feb 2014
+ * @date            Feb 2014
  * @author          Hauke Petersen <hauke.petersen@fu-berlin.de>
  */
 
@@ -17,13 +17,13 @@
  */
 const DEFAULT_HOST = 'localhost';               /* host address for the demo server */
 const DEFAULT_PORT = 11111;                     /* host port for the demo server */
-const DEFAULT_UART = '/dev/ttyUSB0';		        /* defeul serial port connected to a msba2 */
+const DEFAULT_UART = '/dev/ttyUSB0';            /* defeul serial port connected to a msba2 */
 
 /**
  * Library imports
  */
 var net = require('net');
-var	JsonSocket = require('json-socket');
+var JsonSocket = require('json-socket');
 var serialPort = require('serialport');
 var SerialPort = serialPort.SerialPort;
 
@@ -33,8 +33,8 @@ var SerialPort = serialPort.SerialPort;
 var host = DEFAULT_HOST;
 var port = DEFAULT_PORT;
 var dev = DEFAULT_UART;
-var socket = new JsonSocket(new net.Socket());	/* connection to the demo host */
-var isConnected = false;						/* flag signals when connected to the host */
+var socket = new JsonSocket(new net.Socket());  /* connection to the demo host */
+var isConnected = false;                        /* flag signals when connected to the host */
 var uart;
 
 /**
@@ -43,19 +43,19 @@ var uart;
  * Usage: node portal.js [DEV] [HOST] [PORT]
  */
 function parseCommandLineArgs() {
-	process.argv.forEach(function(arg, index) {
-		switch (index) {
-			case 2:
-				dev = arg;
-			break;
-			case 3:
-				host = arg;
-			break;
-			case 4:
-				port = arg;
-			break;
-		}
-	});
+    process.argv.forEach(function(arg, index) {
+        switch (index) {
+            case 2:
+                dev = arg;
+            break;
+            case 3:
+                host = arg;
+            break;
+            case 4:
+                port = arg;
+            break;
+        }
+    });
 }
 
 /**
@@ -63,36 +63,36 @@ function parseCommandLineArgs() {
  * (event handlers for the TCP connection to the host)
  */
 function connect() {
-	if (!isConnected) {
-		console.log("SOCKET: Trying to connect to " + host + ':' + port);
-		socket.connect(port, host);
-	}
+    if (!isConnected) {
+        console.log("SOCKET: Trying to connect to " + host + ':' + port);
+        socket.connect(port, host);
+    }
 }
 
 socket.on('connect', function() {
-	console.log('SOCKET: Successfully connected to ' + host + ':' + port);
-	isConnected = true;
+    console.log('SOCKET: Successfully connected to ' + host + ':' + port);
+    isConnected = true;
 });
 
 socket.on('close', function(error) {
-	if (!error) {
-		console.log('SOCKET: Connection was terminated, we\'ll see each other again.');
-		isConnected = false;
-		connect();
-	}
+    if (!error) {
+        console.log('SOCKET: Connection was terminated, we\'ll see each other again.');
+        isConnected = false;
+        connect();
+    }
 });
 
 socket.on('error', function() {
-	console.log('SOCKET: Unable to connect, next departure in 1s');
-	setTimeout(connect, 1000);
+    console.log('SOCKET: Unable to connect, next departure in 1s');
+    setTimeout(connect, 1000);
 });
 
 /** 
  * Handle incoming traffic (and commands to the MSBA2)
  */
 socket.on('message', function(data) {
-	console.log('DATA:    ' + data.data);
-    uart.write(data.data + "\n");
+    console.log('DATA:    ' + data.data);
+    uart.write("fw " + data.dst + " " + data.id + " " + data.data\n);
 });
 
 /**
@@ -104,12 +104,12 @@ parseCommandLineArgs();
 console.log("INFO:   Opening RIOT at " + dev + " and socket to " + host + ":" + port);
 connect();
 uart = new SerialPort(dev, {
-	'baudrate': 115200, 
-	'databits': 8, 
-	'parity': 'none', 
-	'stopbits': 1, 
-	'parser': serialPort.parsers.readline("\n")},
-	false);										/* connection to the sensor node over UART */
+    'baudrate': 115200, 
+    'databits': 8, 
+    'parity': 'none', 
+    'stopbits': 1, 
+    'parser': serialPort.parsers.readline("\n")},
+    false);                                     /* connection to the sensor node over UART */
 
 
 /**
@@ -118,9 +118,13 @@ uart = new SerialPort(dev, {
  */
 function parseEvent(data) {
     var res = {};
-    res.id = data[0];
-    res.data = data[1];
-    res.time = new Date().getTime();
+    if (data.match(/^FW:\s+\d+\s+\d+\s+\d+\s?/)) {
+        var split = data.split(" ");
+        res.src = parseInt(split[1]);
+        res.id = parseInt(split[2]);
+        res.data = parseInt(split[3]);
+        res.time = new Date().getTime();
+    }
     return res;
 }
 
@@ -129,13 +133,30 @@ function parseEvent(data) {
  * (event handlers for the serial port)
  */
 uart.open(function() {
-	console.log('SERIAL: Connection opened - portal open for data travel');
-	uart.on('data', function(data){
-		var time = new Date().getTime();
-		console.log('UART:    ' + data);
-		if (isConnected) {
+    console.log('SERIAL: Connection opened - portal open for data travel');
+    uart.on('data', function(data){
+        console.log('UART:    ' + data);
+        if (isConnected) {
             data = parseEvent(data);
-			socket.sendMessage(data);
-		}
-	});
+            socket.sendMessage(data);
+        }
+    });
 });
+
+
+// test cases for event parsing
+// var t1 = [
+//     "FW: 12 23 88",
+//     "NO: 12 23 88",
+//     " FW: 32 32 32",
+//     "FW: 986 aa 88",
+//     "-FW: aa 99 23",
+//     "FW: 8 21 0 99",
+//     "FW: 12312 1288381 111",
+//     "23"];
+
+// t1.forEach(function(test){
+//     console.log("Test: " + test);
+//     console.log(parseEvent(test));
+//     console.log("--------");
+// });
